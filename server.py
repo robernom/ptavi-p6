74160663e9523a0243bb7c519973f97a0cacb2ea
robernom <rober95@hotmail.com>
2016-11-14 10:56:32 +0100

    recibe parametros

--- server.py
+++ server.py
@@ -5,27 +5,40 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import sys
+import os 
+
+os.system("clear")
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPHandler(socketserver.DatagramRequestHandler):
     """
-    Echo server class
+    SIP server class
     """
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            # Si no hay más líneas salimos del bucle infinito
+            print("El cliente nos manda " + line.decode('utf-8'))
+            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+
+            
+            
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    # Creamos servidor y escuchamos
+    try:
+        IP, PORT, CANCION = sys.argv[1:]
+    except (IndexError, ValueError):
+        sys.exit("Usage: python3 server.py IP port audio_file")
+    serv = socketserver.UDPServer((IP, int(PORT)), SIPHandler)
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        sys.exit("\r\nClosed")
