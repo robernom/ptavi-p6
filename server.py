9dd6c85c2b5ebdda42ccbf83ae8971de2fdb899b
robernom <rober95@hotmail.com>
2016-11-21 10:14:18 +0100

    pep257

--- server.py
+++ server.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor de eco en UDP simple
-"""
+"""Clase (y programa principal) para un servidor de eco en UDP simple."""
 
 import socketserver
 import sys
 import os
 
-os.system("clear")
-
 
 class SIPHandler(socketserver.DatagramRequestHandler):
-    """
-    SIP server class
-    """
+    """SIP server class."""
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
