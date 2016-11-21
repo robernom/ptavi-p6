8202c4fac60d622c9726217a6e867e73a8276c74
Roberto Nombela Alonso <rnombela@gamma04.aulas.gsyc.urjc.es>
2016-11-21 10:03:09 +0100

    pep8

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
-import os 
+import os
 
 os.system("clear")
 
@@ -20,21 +20,26 @@ class SIPHandler(socketserver.DatagramRequestHandler):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         """Cada vez que un cliente envia una peticion se ejecuta."""
         data = self.rfile.read().decode('utf-8')
+        allowed = ["INVITE", "ACK", "BYE"]
         print(data)
         c_data = data.split()
         c_ip = self.client_address[0]
         met = c_data[0]
+        to_send = ""
         if met == "INVITE":
-        	self.wfile.write(b"SIP/2.0 100 Trying\r\n")
-        	self.wfile.write(b"SIP/2.0 180 Ring\r\n")
-        	self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            to_send = (b"SIP/2.0 100 Trying\r\n\r\n")
+            to_send += (b"SIP/2.0 180 Ring\r\n\r\n")
+            to_send += (b"SIP/2.0 200 OK\r\n\r\n")
         elif met == "BYE":
-            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            to_send = (b"SIP/2.0 200 OK\r\n\r\n")
         elif met == "ACK":
-            print("Envio RTP")
-            os.system("./mp32rtp -i " + c_ip +" -p 23032 < cancion.mp3")
+            os.system("./mp32rtp -i " + c_ip + " -p 23032 < cancion.mp3")
+        elif met not in allowed:
+            to_send = (b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
         else:
-            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+            to_send = (b"SIP/2.0 400 Bad Request\r\n\r\n")
+        if to_send != "":
+            self.wfile.write(to_send)
 
 if __name__ == "__main__":
     # Creamos servidor y escuchamos
