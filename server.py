c8252e807f862c827fb78442752c7c3cfe726d4d
robernom <rober95@hotmail.com>
2016-11-21 17:21:30 +0100

    Terminado

--- server.py
+++ server.py
@@ -11,39 +11,36 @@ class SIPHandler(socketserver.DatagramRequestHandler):
     """SIP server class."""
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
         """Cada vez que un cliente envia una peticion se ejecuta."""
         data = self.rfile.read().decode('utf-8')
         allowed = ["INVITE", "ACK", "BYE"]
         print(data)
-        c_data = data.split()
+        met = data.split()[0]
         c_ip = self.client_address[0]
-        met = c_data[0]
         to_send = ""
         if met == "INVITE":
-            to_send = (b"SIP/2.0 100 Trying\r\n\r\n")
-            to_send += (b"SIP/2.0 180 Ring\r\n\r\n")
-            to_send += (b"SIP/2.0 200 OK\r\n\r\n")
+            to_send = ("SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\n" +
+                        "SIP/2.0 200 OK\r\n\r\n")
         elif met == "BYE":
-            to_send = (b"SIP/2.0 200 OK\r\n\r\n")
+            to_send = "SIP/2.0 200 OK\r\n\r\n"
         elif met == "ACK":
-            os.system("./mp32rtp -i " + c_ip + " -p 23032 < cancion.mp3")
+            os.system("./mp32rtp -i " + c_ip + " -p 23032 <" + SONG)
         elif met not in allowed:
-            to_send = (b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+            to_send = "SIP/2.0 405 Method Not Allowed\r\n\r\n"
         else:
-            to_send = (b"SIP/2.0 400 Bad Request\r\n\r\n")
+            to_send = "SIP/2.0 400 Bad Request\r\n\r\n"
         if to_send != "":
-            self.wfile.write(to_send)
+            self.wfile.write(bytes(to_send, 'utf-8'))
 
 if __name__ == "__main__":
     # Creamos servidor y escuchamos
     try:
-        IP, PORT, CANCION = sys.argv[1:]
+        IP, PORT, SONG = sys.argv[1:]
     except (IndexError, ValueError):
         sys.exit("Usage: python3 server.py IP port audio_file")
-    serv = socketserver.UDPServer((IP, int(PORT)), SIPHandler)
+    SERV = socketserver.UDPServer((IP, int(PORT)), SIPHandler)
     print("Listening...")
     try:
-        serv.serve_forever()
+        SERV.serve_forever()
     except KeyboardInterrupt:
         sys.exit("\r\nClosed")
